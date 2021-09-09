b715a0cd0b005ceea9e7bcebca218084f2e038d1
WooSung Kim <sibu2005@naver.com>
2021-09-09 20:14:19 +0900

    restore blog

---
+++ src/components/Common/App1.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+
+
+const Text1 = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+`
+
+
+const BlogTittle: FunctionComponent = function(){
+  return (
+    <div></div>
+  )
+}
+
+
+export default BlogTittle;
