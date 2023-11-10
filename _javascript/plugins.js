82c5d6fe1d6a3949a63b5b89d8452ffb8525eabf
Woosung_Kim <sibu2005@naver.com>
2023-11-10 22:55:10 +0900

    fix : 빌드 실패 해결

---
+++ _javascript/plugins.js
@@ -0,0 +1,7 @@
+export { categoryCollapse } from './modules/components/category-collapse';
+export { initClipboard } from './modules/components/clipboard';
+export { imgLazy } from './modules/components/img-lazyload';
+export { imgPopup } from './modules/components/img-popup';
+export { initLocaleDatetime } from './modules/components/locale-datetime';
+export { initPageviews } from './modules/components/pageviews';
+export { toc } from './modules/components/toc';
